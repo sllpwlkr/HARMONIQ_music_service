5552d93111a812511a5d520747d7a4094b0f3298
sllpwlkr <116644736+sllpwlkr@users.noreply.github.com>
2024-12-12 21:22:36 +0300

    Create dml.sql

---
+++ migrations/dml.sql
@@ -0,0 +1,20 @@
+INSERT INTO roles (role_id, name) VALUES
+(1, 'Admin'),
+(2, 'User');
+
+
+-- Добавление действий
+INSERT INTO actions (name) VALUES 
+('Add song'), --1
+('Delete song'), --2
+('Add artist'), --3
+('Delete artist'), --4
+('Add genre'), --5
+('Delete genre'), --6
+('Registration'), --7
+('Add playlist'), --8
+('Delete playlist'), --9
+('Add song in playlist'), --10
+('Delete song from playlist'); --11
+
+
